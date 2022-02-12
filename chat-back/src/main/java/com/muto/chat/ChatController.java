1b998cd37d97a18e9bfc631132f648baa50ccffb
parkmuhyeun <pjhg410@gmail.com>
2022-02-12 23:54:17 +0900

    chat-back

---
+++ chat-back/src/main/java/com/muto/chat/ChatController.java
@@ -0,0 +1,40 @@
+package com.muto.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    //귓속말 할때 사용ㅋㅋ
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat) {
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+
+}
